9c2bf85ed3842bf5f7e9cb8ba900094b58ec0294
DharmaMithra <dharmarockz2001@gmail.com>
2024-11-06 18:24:15 -0600

    Initial Commit

---
+++ facebookLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  facebookLogin
+//
+//  Created by Srikanth Kyatham on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ facebookLoginTests/facebookLoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLoginTests.swift
+//  facebookLoginTests
+//
+//  Created by Srikanth Kyatham on 11/6/24.
+//
+
+import Testing
+@testable import facebookLogin
+
+struct facebookLoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
